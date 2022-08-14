9f923a837485bebc07e39f7d376af166a5180040
ashiqur31 <ashiqur31@gmail.com>
2022-08-14 18:12:06 +0530

    Update account.js

--- email/account.js
+++ email/account.js
@@ -19,7 +19,7 @@ const sendWelcomeEmail = (email, name, phone, message) => {
     
 
     const msg = {
-        to: 's.medhi1@gmail.com',
+        to: 'marketing@brokerinblue.com',
         from: 'ashiqur31@gmail.com', // Change to your verified sender
         subject: 'Brokerinblue: Submitted details',
         // text: 'and easy to do anywhere, even with Node.js',
